c67cd09cdd1bc26eb4ef5ba41db195003c43b7f2
U200709069 <72610534+berkezkul@users.noreply.github.com>
2024-04-21 23:06:18 +0300

    vector

--- embedding.py
+++ embedding.py
@@ -12,20 +12,20 @@ class Embedder:
         #self.client = OpenAI(api_key=api_key)# OpenAI api_key'i ayarla
 
     def generate_embeddings(self, texts: List[str]) -> List[dict]:
-        """OpenAI API kullanarak metinler için gömme vektörler üretir."""
+        """OpenAI API kullanarak metinler için embedding vektörleri üretme"""
         try:
-            # OpenAI ile gömme vektörleri oluştur
 
-            embeddings_responses = openai.embeddings.create(
+
+            response = openai.embeddings.create(
                 model="text-embedding-ada-002",
                 input=texts  # Birden fazla metin gönderiliyor
-            ).data[0]
+            )
             # Yanıttan gerekli bilgiyi çek
 
-            embeddings = [response for response in embeddings_responses]
-            if len(embeddings) == 0:
-                print("embeddings bozuk")
-            # Qdrant'ın beklediği formata dönüştür
+            #embeddings = [response for response in embeddings_responses]
+            embeddings = response['data'][0]['embedding']
+
+            # Qdrant'ın beklediği format
             qdrant_embeddings = [{'vector': embedding} for embedding in embeddings]
             return qdrant_embeddings
         except openai.OpenAIError as e:
